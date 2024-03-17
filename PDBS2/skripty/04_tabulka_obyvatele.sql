13b9fa83a160c1aa001bbf7e016edc867894dcd1
Jan Caha <jan.caha@outlook.com>
2024-03-17 14:13:28 +0100

    data

---
+++ PDBS2/skripty/04_tabulka_obyvatele.sql
@@ -0,0 +1,21 @@
+-- Table: public.obyvatele_roky
+
+-- DROP TABLE IF EXISTS public.obyvatele_roky;
+
+CREATE TABLE IF NOT EXISTS public.obyvatele_roky
+(
+    idhod integer,
+    hodnota integer,
+    stapro_kod integer,
+    pohlavi_cis boolean,
+    pohlavi_kod boolean,
+    vek_cis boolean,
+    vek_kod boolean,
+    uzemi_cis integer,
+    uzemi_kod integer,
+    obdobi text COLLATE pg_catalog."default",
+    pohlavi_txt boolean,
+    vek_txt boolean,
+    uzemi_txt text COLLATE pg_catalog."default",
+    uzemi_typ text COLLATE pg_catalog."default"
+)
