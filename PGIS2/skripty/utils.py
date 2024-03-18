ce8d1a89f41d44eacfbfa6a98898f4dbe0afb905
Jan Caha <jan.caha@outlook.com>
2024-03-18 10:37:24 +0100

    fix

--- PGIS2/skripty/utils.py
+++ PGIS2/skripty/utils.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 
 def data_path(filename: str) -> Path:
-    file = Path(__file__).parent.parent / "_data" / filename)
+    file = Path(__file__).parent.parent / "_data" / filename
     return file
