eb964ed047802bc53f678b63679be819a1a78f72
Jan Caha <jan.caha@outlook.com>
2024-04-15 19:14:45 +0200

    update

--- PGIS2/skripty/08_objekt.py
+++ PGIS2/skripty/08_objekt.py
@@ -1,9 +1,17 @@
+import typing
+
+
 class Zamestnanec:
     """Třída reprezentující zaměstnance. Zaměstnanec má jméno a hodinový plat."""
 
     def __init__(self, jmeno: str, hodinovy_plat: float):
         self.jmeno = jmeno
         self._hodinovy_plat = hodinovy_plat
+        self._srazka_z_platu: typing.Optional[float] = None
+
+    def nastav_srazku_z_platu(self, srazka: typing.Optional[float] = None) -> None:
+        """Funkce nastaví sražku z platu zaměstnance."""
+        self._srazka_z_platu = srazka
 
     def plat(self) -> float:
         """Funkce vrátí hodinový plat zaměstnance."""
@@ -19,12 +27,24 @@ class Zamestnanec:
 
     def mesicni_plat(self, odpracovanych_hodin: float) -> float:
         """Funkce vrátí měsíční plat zaměstnance za zadaný počet odpracovaných hodin."""
-        return self._hodinovy_plat * odpracovanych_hodin
+        if self._srazka_z_platu:
+            return self._hodinovy_plat * odpracovanych_hodin - self._srazka_z_platu
+        else:
+            return self._hodinovy_plat * odpracovanych_hodin
 
     @property
     def denni_plat(self) -> float:
         """Property vrací denní plat zaměstnance za 8 hodin práce."""
-        return self._hodinovy_plat / 8
+        return self._hodinovy_plat * 8
+
+    def __repr__(self) -> str:
+        return f"{self.jmeno} s denním platem {self._hodinovy_plat}."
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, Zamestnanec):
+            return self.jmeno == value.jmeno
+        else:
+            raise NotImplementedError
 
 
 if __name__ == "__main__":
--- PGIS2/skripty/utils.py
+++ PGIS2/skripty/utils.py
@@ -1,6 +1,21 @@
+import warnings
 from pathlib import Path
 
 
 def data_path(filename: str) -> Path:
+    """Returns a path to a file as a Path object or raises an error if file does not exist."""
     file = Path(__file__).parent.parent / "_data" / filename
+    if not file.exists():
+        raise ValueError(f"File {file.as_posix()} does not exist.")
     return file
+
+
+def get_save_data_path(filename: str, delete_if_exist: bool = False) -> Path:
+    """Returns a path to a file as a Path object. If the file exists, it prints a warning. Optionally, the file can be deleted."""
+    path = Path(__file__).parent / "data" / "saved" / filename
+    if path.exists():
+        warnings.warn(f"File {path.as_posix()} exists.")
+        if delete_if_exist:
+            warnings.warn("File was deleted!")
+            path.unlink()
+    return path
