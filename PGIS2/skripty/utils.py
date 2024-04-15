0beaa44f63a69a9d3b354db35446d1c811fecba9
Jan Caha <jan.caha@outlook.com>
2024-04-15 21:54:07 +0200

    fixes

--- PGIS2/skripty/utils.py
+++ PGIS2/skripty/utils.py
@@ -2,17 +2,22 @@ import warnings
 from pathlib import Path
 
 
+def base_data_path() -> Path:
+    """Returns a path to the data directory as a Path object."""
+    return Path(__file__).parent.parent / "_data"
+
+
 def data_path(filename: str) -> Path:
     """Returns a path to a file as a Path object or raises an error if file does not exist."""
-    file = Path(__file__).parent.parent / "_data" / filename
+    file = base_data_path() / filename
     if not file.exists():
         raise ValueError(f"File {file.as_posix()} does not exist.")
     return file
 
 
-def get_save_data_path(filename: str, delete_if_exist: bool = False) -> Path:
+def save_data_path(filename: str, delete_if_exist: bool = False) -> Path:
     """Returns a path to a file as a Path object. If the file exists, it prints a warning. Optionally, the file can be deleted."""
-    path = Path(__file__).parent.parent / "_data" / "saved" / filename
+    path = base_data_path() / "saved" / filename
     if not path.parent.exists():
         path.parent.mkdir(parents=True)
     if path.exists():
