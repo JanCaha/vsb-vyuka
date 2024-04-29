ffc62ecba480234c04d9872165243b4fb682507f
Jan Caha <jan.caha@outlook.com>
2024-04-29 18:33:12 +0200

    additions to script

--- PGIS2/skripty/09_polars.py
+++ PGIS2/skripty/09_polars.py
@@ -1,5 +1,16 @@
+import typing
+
 import polars as pl
 from utils import data_path, save_data_path
+
+
+def calculate_aggregates(column_name: str) -> typing.List[pl.Expr]:
+    """Vytvoří výrazy pro výpočet mediánu a průměru pro zadaný sloupec."""
+    expressions = []
+    expressions.append(pl.col(column_name).median().alias(f"median_{column_name}"))
+    expressions.append(pl.col(column_name).mean().alias(f"mean_{column_name}"))
+    return expressions
+
 
 if __name__ == "__main__":
     df = pl.read_csv(data_path("diamonds.csv"))
@@ -21,7 +32,8 @@ if __name__ == "__main__":
     price = df_diamonds_e.get_column("price").to_list()
 
     df_diamonds_grouped = df.groupby(["cut", "color"]).agg(
-        pl.col("price").median().alias("median_price"), pl.col("price").mean().alias("mean_price")
+        calculate_aggregates("price"),
+        calculate_aggregates("x"),
     )
 
     print(df_diamonds_grouped)
