e401cc2a9a16e326feeb1b281197394cdc806993
Jan Caha <jan.caha@outlook.com>
2024-04-29 18:43:47 +0200

    fix script and add opposite step

--- PDBS2/skripty/02_load_data_to_postgis.sh
+++ PDBS2/skripty/02_load_data_to_postgis.sh
@@ -1,4 +1,7 @@
-PG_CON=Pg:"dbname=postgres host=localhost user=postgres port=5432 password=heslo"
+# uložení vrstvy z GPKG do PostGIS
+ogr2ogr -nln kraje Pg:"dbname=postgres host=localhost user=postgres port=5432 password=heslo" uzemni_jednotky.gpkg kraje
+ogr2ogr -nln okresy Pg:"dbname=postgres host=localhost user=postgres port=5432 password=heslo" uzemni_jednotky.gpkg okresy
 
-ogr2ogr -nln kraje $PG_CON uzemni_jednotky.gpkg kraje
-ogr2ogr -nln okresy $PG_CON uzemni_jednotky.gpkg okresy
+# uložení dat z PostGIS do souboru
+ogr2ogr okresy.geojson Pg:"dbname=postgres host=localhost user=postgres port=5432 password=heslo" okresy
+ogr2ogr okresy.gpkg Pg:"dbname=postgres host=localhost user=postgres port=5432 password=heslo" okresy
