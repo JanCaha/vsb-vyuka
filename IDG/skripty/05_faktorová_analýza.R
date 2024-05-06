189042334c72977c5ba80c00bce6c6d6f000bf27
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-05-06 12:18:25 +0000

    Built site for gh-pages

---
+++ IDG/skripty/05_faktorová_analýza.R
@@ -0,0 +1,31 @@
+# knihovny ----------------------------------------------------------------
+
+library(tidyverse)
+library(performance) # easystats
+library(parameters) # easystats
+
+# příprava data -----------------------------------------------------------
+
+data(iris)
+
+iris_selected <- iris %>% 
+  select(1:4)
+
+# kontrola vhodnosti ------------------------------------------------------
+
+performance::check_factorstructure(iris_selected)
+
+# počet faktorů -----------------------------------------------------------
+
+factors_n <- n_factors(iris_selected, type = "FA")
+
+factors_n
+
+as.data.frame(factors_n)
+
+# faktorová analýza -------------------------------------------------------
+
+iris_fa <- psych::fa(iris_selected, nfactors = 2) %>%
+  model_parameters(sort = TRUE, threshold = "max")
+
+iris_fa
