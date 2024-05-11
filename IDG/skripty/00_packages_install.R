40192bd66bbccd8aff934a2149df37ec71c34eb9
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-05-11 22:27:21 +0000

    Built site for gh-pages

--- IDG/skripty/00_packages_install.R
+++ IDG/skripty/00_packages_install.R
@@ -1,4 +1,4 @@
-install.packages(c(
+needed_packages <- c(
     "tidyverse",
     "here",
     "sf",
@@ -14,6 +14,13 @@ install.packages(c(
     "plotly",
     "tidymodels",
     "easystats"
-))
+)
+
+# nejjednodušší způsob instalace balíčků by bylo prosté volání install.packages(needed_packages),
+# ale to by znamenalo, že by se balíčky instalovaly i když už jsou nainstalované, což je zbytečné a zdlouhavé
+# následující kód tedy instaluje jen ty balíčky, které ještě nejsou nainstalované, ale kód je komplikovanější
+
+new_packages <- needed_packages[!(needed_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
 
 remotes::install_github("JanCaha/r_package_qgis")
