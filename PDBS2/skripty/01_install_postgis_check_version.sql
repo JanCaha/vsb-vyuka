6f1b265e2f6ca5a0ff7872ca960a67b0f3a63bee
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-05-12 20:23:51 +0000

    Built site for gh-pages

--- PDBS2/skripty/01_install_postgis_check_version.sql
+++ PDBS2/skripty/01_install_postgis_check_version.sql
@@ -1,5 +1,5 @@
-CREATE EXTENSION  IF NOT EXISTS postgis;
-CREATE EXTENSION  IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
 
 -- upgrade from previous versions
 --SELECT postgis_extensions_upgrade();
