c4301f300788f56d14654ba8660c0d3332ae974c
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-05-13 07:03:00 +0000

    Built site for gh-pages

--- PDBS2/skripty/09_geometry_scale.sql
+++ PDBS2/skripty/09_geometry_scale.sql
@@ -1,3 +1,5 @@
+DROP VIEW IF EXISTS okresy_scaled;
+
 CREATE VIEW okresy_scaled AS (
 	WITH okresy_ratio AS (
 		WITH okresy_rozloha AS (
@@ -9,4 +11,4 @@ CREATE VIEW okresy_scaled AS (
 		SELECT nazev, rozloha, rozloha/max_rozloha.value as ratio, geom, ST_Centroid(geom) as centroid FROM okresy_rozloha, max_rozloha
 	)
 	SELECT rozloha, ratio, nazev, ST_Translate(ST_Scale(ST_Translate(geom, -ST_X(centroid),-ST_Y(centroid)), ratio, ratio), ST_X(centroid), ST_Y(centroid)) as geom FROM okresy_ratio
-)
+);
