2dd2d29fcf8d5c327db9f1c936ebfd83584c6d2f
Jan Caha <jan.caha@outlook.com>
2024-09-02 10:32:48 +0200

    fix

--- linux_install_R.sh
+++ linux_install_R.sh
@@ -49,13 +49,13 @@ echo "Install ggplot2 package - $GGPLOT2"
 
 # Prepare and install R
 sudo wget -q https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc -O /etc/apt/trusted.gpg.d/marutter_pubkey.asc
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/marutter_pubkey.asc] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" | sudo tee /etc/apt/sources.list.d/r.list
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/marutter_pubkey.asc] https://cloud.r-project.org/bin/linux/ubuntu $UBUNTU_CODENAME-cran40/" | sudo tee /etc/apt/sources.list.d/r.list
 sudo apt-get update
 sudo apt-get install -y r-base
 
 # Prepare and install R packages from binaries (otherwise it takes forever to compile them from source)    
 sudo wget -q https://eddelbuettel.github.io/r2u/assets/dirk_eddelbuettel_key.asc -O /etc/apt/trusted.gpg.d/cranapt_key.asc
-echo "deb [arch=amd64] https://r2u.stat.illinois.edu/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/cranapt.list
+echo "deb [arch=amd64] https://r2u.stat.illinois.edu/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/cranapt.list
 sudo apt-get update
 
 if [ "$ALL_PACKAGES" = true ]; then
