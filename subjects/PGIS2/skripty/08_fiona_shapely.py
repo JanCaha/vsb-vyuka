74f5eb6f003c91268db2601a070a851ca0d1c753
Jan Caha <jan.caha@outlook.com>
2025-03-31 14:35:09 +0200

    create error

--- subjects/PGIS2/skripty/08_fiona_shapely.py
+++ subjects/PGIS2/skripty/08_fiona_shapely.py
@@ -7,6 +7,9 @@ from shapely.geometry import MultiPolygon, mapping, shape
 from shapely.ops import transform
 
 if __name__ == "__main__":
+    import sys
+
+    sys.exit(100)
 
     path_data = utils.data_path("ne_10m_admin_0_countries.shp")
     path_result_file = Path(__file__).parent / "data.gpkg"
