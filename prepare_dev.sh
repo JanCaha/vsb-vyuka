954eb8d84f05481d93841b1ba691e4bd162c7c95
Jan Caha <jan.caha@outlook.com>
2025-04-02 05:25:16 +0200

    also install

--- prepare_dev.sh
+++ prepare_dev.sh
@@ -5,4 +5,6 @@ BASE_DIR=$(dirname "$(realpath "$0")")
 # python balíček pro PGIS2 
 cd dev
 zip -r "../subjects/PGIS2/skripty/python_balicek.zip" "balicekpgis2"
+cd balicekpgis2
+pip install .
 cd $BASE_DIR
