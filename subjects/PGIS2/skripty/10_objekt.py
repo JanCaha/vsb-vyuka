1c945415e242844a1e4c4fe3c7aaef8f717b1b90
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-04-02 03:29:36 +0000

    Built site for gh-pages

--- subjects/PGIS2/skripty/10_objekt.py
+++ subjects/PGIS2/skripty/10_objekt.py
@@ -7,11 +7,7 @@ class Zamestnanec:
     def __init__(self, jmeno: str, hodinovy_plat: float):
         self.jmeno = jmeno
         self._hodinovy_plat = hodinovy_plat
-        self._srazka_z_platu: typing.Optional[float] = None
-
-    def nastav_srazku_z_platu(self, srazka: typing.Optional[float] = None) -> None:
-        """Funkce nastaví sražku z platu zaměstnance."""
-        self._srazka_z_platu = srazka
+        self.uvazek = 1.0
 
     def plat(self) -> float:
         """Funkce vrátí hodinový plat zaměstnance."""
@@ -27,18 +23,26 @@ class Zamestnanec:
 
     def mesicni_plat(self, odpracovanych_hodin: float) -> float:
         """Funkce vrátí měsíční plat zaměstnance za zadaný počet odpracovaných hodin."""
-        if self._srazka_z_platu:
-            return self._hodinovy_plat * odpracovanych_hodin - self._srazka_z_platu
-        else:
-            return self._hodinovy_plat * odpracovanych_hodin
+        return self._hodinovy_plat * odpracovanych_hodin
+
+    ## statická metoda, jedná se o ukázku, ale zde by mohla být i normální metoda
+    @staticmethod
+    def delka_pracovniho_dne(uvazek: float = 1.0) -> float:
+        """Statická metoda vrací délku pracovního dne zaměstnance dle výše úvazku."""
+        return 8 * uvazek
+
+    @property
+    def pracovnich_hodin_denne(self) -> float:
+        """Vrací délku pracovního dne zaměstnance dle výše úvazku."""
+        return self.delka_pracovniho_dne(self.uvazek)
 
     @property
     def denni_plat(self) -> float:
         """Property vrací denní plat zaměstnance za 8 hodin práce."""
-        return self._hodinovy_plat * 8
+        return self._hodinovy_plat * self.pracovnich_hodin_denne
 
     def __repr__(self) -> str:
-        return f"{self.jmeno} s denním platem {self._hodinovy_plat}."
+        return f"{self.jmeno} s denním platem {self.denni_plat}."
 
     def __eq__(self, value: object) -> bool:
         if isinstance(value, Zamestnanec):
@@ -48,8 +52,16 @@ class Zamestnanec:
 
 
 if __name__ == "__main__":
-    odpracovane_hodiny = 160
+    # tvorba instance třídy zamestnanec
     zam = Zamestnanec("Testovaci Zamestnanec", 250)
+    # nastení odpracovaných hodin
+    odpracovane_hodiny = 160
+    print(f"Denní plat: {zam.denni_plat}")
     print(f"Plat: {zam.mesicni_plat(odpracovane_hodiny)}")
+    # navýšení platu
     zam.zvys_plat(50)
     print(f"Plat po zvýšení: {zam.mesicni_plat(odpracovane_hodiny)}")
+
+    # změna úvazku
+    zam.uvazek = 0.5
+    print(f"Denní plat: {zam.denni_plat} při úvazku {zam.uvazek}")
