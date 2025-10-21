e4ee7f49f671e106dc2c5fbd49c175312dd14f9a
Jan Caha <jan.caha@outlook.com>
2025-10-21 13:34:59 +0200

    update

--- subjects/PGIS3/skripty/07_geometrie_mereni.py
+++ subjects/PGIS3/skripty/07_geometrie_mereni.py
@@ -8,8 +8,8 @@ crs_sjtsk = arcpy.SpatialReference(5514)
 print(f"{crs_wgs84.type}: {crs_wgs84.angularUnitName} - {crs_wgs84.linearUnitName}")
 print(f"{crs_sjtsk.type}: {crs_sjtsk.angularUnitName} - {crs_sjtsk.linearUnitName}")
 
-print(utils.is_projected(crs_wgs84))
-print(utils.is_projected(crs_sjtsk))
+print(f"WGS 84 je projektovaný: {utils.is_projected(crs_wgs84)}")
+print(f"S-JTSK je projektovaný: {utils.is_projected(crs_sjtsk)")
 
 line = arcpy.Polyline(
     arcpy.Array(
@@ -23,11 +23,15 @@ line = arcpy.Polyline(
 
 # délka v WGS84 nedává smysl - úhlové jednotky
 print(line.WKT)
-print(line.length)
+print(f"Délka linie v úhlových jednotkách: {line.length}")
 
 # konverze do S-JTSK
 line_sjtsk: arcpy.Geometry = line.projectAs(crs_sjtsk)
 
 # délka v S-JTSK dává smysl - metry, vyděleno 1000 pro km
 print(line_sjtsk.WKT)
-print(line_sjtsk.length / 1000)
+print(f"Délka linie v projektovaných jednotkách: {line_sjtsk.length / 1000}")
+
+# výpočty pomocí getLength()
+print(f"Délka při výpočtu GEODESIC v S-JTSK: {line_sjtsk.getLength('GEODESIC', 'Kilometers')}")
+print(f"Délka při výpočtu GEODESIC v WGS-84: {line.getLength('GEODESIC', 'Kilometers')}")
--- subjects/PGIS3/skripty/08_geometrie_vztahy.py
+++ subjects/PGIS3/skripty/08_geometrie_vztahy.py
@@ -1,3 +1,5 @@
+import math
+
 import arcpy
 
 # geometrie čtverce
@@ -25,27 +27,49 @@ triangle = arcpy.Polygon(
     )
 )
 
+print("===WKT===")
 # výpis geometrie
 print(rectangle.WKT)
 print(triangle.WKT)
 
+print("===Shodnost/Neshodnost===")
 # kontroly shodnosti geometrií
 print(rectangle == triangle)
 print(rectangle != triangle)
 
+print("===Průnik===")
 # prostorové vztahy - průnik
-intersect: arcpy.Geometry = rectangle.intersect(triangle, 1)
+intersect: arcpy.Geometry = rectangle.intersect(triangle, 4)
 print(intersect.WKT)
 
+print("===Sjednocení===")
 # prostorové vztahy - sjednocení - dva způsoby zápisu jeden pomocí metody, druhý pomocí operátoru
 union: arcpy.Geometry = rectangle.union(triangle)
-union: arcpy.Geometry = rectangle | triangle
+# union: arcpy.Geometry = rectangle | triangle
 print(union.WKT)
 
+print("===Rozdíl===")
 # prostorové vztahy - rozdíl
 difference: arcpy.Geometry = rectangle.difference(triangle)
 print(difference.WKT)
 
+print("===Symetrický Rozdíl===")
 # prostorové vztahy - symetrický rozdíl
 symDiff: arcpy.Geometry = rectangle.symmetricDifference(triangle)
 print(symDiff.WKT)
+
+print("===Rotace kolem 0,0===")
+rotate1: arcpy.Geometry = rectangle.rotate(rotation_angle=math.radians(90))
+print(rotate1.WKT)
+
+print("===Rotace kolem 0.5,0.5===")
+rotate2: arcpy.Geometry = rectangle.rotate(arcpy.Point(0.5, 0.5), rotation_angle=math.radians(45))
+print(rotate2.WKT)
+
+print("===Zmenšení kolem 0,0===")
+scale1: arcpy.Geometry = rectangle.scale(sx=0.5, sy=0.5)
+print(scale1.WKT)
+
+print("===Zmenšení kolem 0.5,0.5===")
+scale2: arcpy.Geometry = rectangle.scale(arcpy.Point(0.5, 0.5), sx=0.5, sy=0.5)
+print(scale2.WKT)
