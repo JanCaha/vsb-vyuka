73641a9cb2e59781c0db757b482cac2e1385face
Jan Caha <jan.caha@outlook.com>
2025-11-11 10:24:47 +0100

    remove file and then recreate it

--- prepare_dev.sh
+++ prepare_dev.sh
@@ -4,14 +4,17 @@ BASE_DIR=$(dirname "$(realpath "$0")")
 
 # python balíček pro PGIS2 
 cd dev
-zip -r "../subjects/PGIS2/skripty/python_balicek.zip" "balicekpgis2"
+FILE="../subjects/PGIS2/skripty/python_balicek.zip"
+rm
+zip -r $FILE "balicekpgis2"
 cd balicekpgis2
 pip install .
 cd $BASE_DIR
 
 # python balíček pro PGIS3 
 cd dev
-zip -r "../subjects/PGIS3/skripty/balik_pgis3.zip" "balik_pgis3"
+FILE="../subjects/PGIS3/skripty/balik_pgis3.zip"
+zip -r $FILE "balik_pgis3"
 cd balik_pgis3
 pip install .
 cd $BASE_DIR
