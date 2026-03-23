54f28c296b8e741c2e08767b428453979f428c24
Jan Caha <jan.caha@outlook.com>
2026-03-23 08:38:14 +0100

    update package

--- baliky/balik_pgis2/src/balicekpgis2/__init__.py
+++ baliky/balik_pgis2/src/balicekpgis2/__init__.py
@@ -1,3 +1,4 @@
 from .context_managers import BandAsArrayContextManager, LayerContextManager, LayerFromDatasetContextManager
 from .data import data_countries, data_shaded_relief
+from .local_data import data_file_path, read_text_file
 from .paths import PathManager
