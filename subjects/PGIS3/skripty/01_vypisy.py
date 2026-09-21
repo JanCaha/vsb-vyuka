d719eed87c001d517ae41815884b747e7af6a753
Jan Caha <jan.caha@outlook.com>
2026-09-21 10:50:04 +0200

    better prints

--- subjects/PGIS3/skripty/01_vypisy.py
+++ subjects/PGIS3/skripty/01_vypisy.py
@@ -4,18 +4,24 @@ import utils
 
 # vypis všech env proměnných
 env_variables = arcpy.ListEnvironments()
+print("Existující proměnné prostředí:")
 print(env_variables)
+print("")
 
 # nastavení workspace
 utils.setup_env("ProgrammingPro/Databases/Trippville_GIS.gdb")
 
 # výpis konkrétní env proměnné
+print("Workspace nastaven na:")
 print(arcpy.env["workspace"])
 print(arcpy.env.workspace)
+print("")
 
 # reset env proměnných
 arcpy.ResetEnvironments()
+print("Po resetu env proměnných je workspace:")
 print(arcpy.env.workspace)
+print("")
 
 # znovu nastavení workspace
 utils.setup_env("ProgrammingPro/Databases/Trippville_GIS.gdb")
@@ -23,9 +29,13 @@ utils.setup_env("ProgrammingPro/Databases/Trippville_GIS.gdb")
 # výpis všech rastrů ve workspace
 rasters = arcpy.ListRasters()
 
+print("Existující rastry ve workspace:")
 print(rasters)
+print("")
 
 # výpis všech vektorů ve workspace se specifickým vzorem v názvu
 vectors = arcpy.ListFeatureClasses("*_P*")
 
+print("Existující vektory ve workspace se specifickým vzorem v názvu:")
 print(vectors)
+print("")
